aeb331dc3ae4e39eed78fcff7b24cb2e573dfcb6
kositzinsky <kositzinsky@gmail.com>
2025-11-20 14:23:54 +0500

    fix post_detail func

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseNotFound
 
 posts = [
     {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': '''Наш корабль, застигнутый в открытом море
+        "id": 0,
+        "location": "Остров отчаянья",
+        "date": "30 сентября 1659 года",
+        "category": "travel",
+        "text": """Наш корабль, застигнутый в открытом море
                 страшным штормом, потерпел крушение.
                 Весь экипаж, кроме меня, утонул; я же,
                 несчастный Робинзон Крузо, был выброшен
                 полумёртвым на берег этого проклятого острова,
-                который назвал островом Отчаяния.''',
+                который назвал островом Отчаяния.""",
     },
     {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
+        "id": 1,
+        "location": "Остров отчаянья",
+        "date": "1 октября 1659 года",
+        "category": "not-my-day",
+        "text": """Проснувшись поутру, я увидел, что наш корабль сняло
                 с мели приливом и пригнало гораздо ближе к берегу.
                 Это подало мне надежду, что, когда ветер стихнет,
                 мне удастся добраться до корабля и запастись едой и
@@ -28,36 +28,46 @@ posts = [
                 Мне всё думалось, что, останься мы на корабле, мы
                 непременно спаслись бы. Теперь из его обломков мы могли бы
                 построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',
+                гиблого места.""",
     },
     {
-        'id': 2,
-        'location': 'Остров отчаянья',
-        'date': '25 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Всю ночь и весь день шёл дождь и дул сильный
+        "id": 2,
+        "location": "Остров отчаянья",
+        "date": "25 октября 1659 года",
+        "category": "not-my-day",
+        "text": """Всю ночь и весь день шёл дождь и дул сильный
                 порывистый ветер. 25 октября.  Корабль за ночь разбило
                 в щепки; на том месте, где он стоял, торчат какие-то
                 жалкие обломки,  да и те видны только во время отлива.
                 Весь этот день я хлопотал  около вещей: укрывал и
-                укутывал их, чтобы не испортились от дождя.''',
+                укутывал их, чтобы не испортились от дождя.""",
     },
 ]
 
 
 def index(request):
-    template = 'blog/index.html'
-    context = {'posts': posts[::-1]}
+    template = "blog/index.html"
+    context = {"posts": posts[::-1]}
     return render(request, template_name=template, context=context)
 
 
-def post_detail(request, pk):
-    template = 'blog/detail.html'
-    context = {'post': posts[pk]}
-    return render(request, template_name=template, context=context)
+def post_detail(request, post_id):
+    template = "blog/detail.html"
+    try:
+        if post_id not in [post["id"] for post in posts]:
+            raise ValueError("Post with this id does not exist")
+        
+        for i in range(len(posts)):
+            if posts[i]["id"] == post_id:
+                context = {"post": posts[post_id]}
+                break
+        return render(request, template_name=template, context=context)
+    except ValueError:
+        return HttpResponseNotFound(request)
+
 
 
 def category_posts(request, category_slug):
-    template = 'blog/category.html'
-    context = {'category': category_slug}
+    template = "blog/category.html"
+    context = {"category": category_slug}
     return render(request, template_name=template, context=context)
